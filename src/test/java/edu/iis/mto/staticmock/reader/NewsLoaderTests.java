3051743fca2f0ec0ba4e79d3397618458f36ab78
patrykxk <patrix215@gmail.com>
2017-04-02 16:19:31 +0200

    Dodano klasę NewsLoaderTests i dodanie dependencies mockito

---
+++ src/test/java/edu/iis/mto/staticmock/reader/NewsLoaderTests.java
@@ -0,0 +1,14 @@
+package edu.iis.mto.staticmock.reader;
+
+import edu.iis.mto.staticmock.ConfigurationLoader;
+import edu.iis.mto.staticmock.NewsReaderFactory;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+
+public class NewsLoaderTests {
+    private ConfigurationLoader configurationLoader = mock(ConfigurationLoader.class);
+    private NewsReaderFactory newsReaderFactory = mock(NewsReaderFactory.class);
+
+
+}
