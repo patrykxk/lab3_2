8bfd26ea50940ad0913cd748e1df83c18d50459b
patrykxk <patrix215@gmail.com>
2017-04-02 16:39:15 +0200

    Dodano dependency PowerMock i jego uzycie w tescie

--- src/test/java/edu/iis/mto/staticmock/reader/NewsLoaderTests.java
+++ src/test/java/edu/iis/mto/staticmock/reader/NewsLoaderTests.java
@@ -3,12 +3,30 @@ package edu.iis.mto.staticmock.reader;
 import edu.iis.mto.staticmock.ConfigurationLoader;
 import edu.iis.mto.staticmock.NewsReaderFactory;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
 import static org.mockito.Mockito.*;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest( ConfigurationLoader.class )
 public class NewsLoaderTests {
-    private ConfigurationLoader configurationLoader = mock(ConfigurationLoader.class);
-    private NewsReaderFactory newsReaderFactory = mock(NewsReaderFactory.class);
+
+    @Test
+    public void test(){
+        mockStatic(ConfigurationLoader.class);
+        ConfigurationLoader configurationLoader = mock(ConfigurationLoader.class);
+        when(ConfigurationLoader.getInstance()).thenReturn(configurationLoader);
+
+        mockStatic(NewsReaderFactory.class);
+        NewsReaderFactory newsReaderFactory = mock(NewsReaderFactory.class);
+        when(NewsReaderFactory.getReader()).thenReturn(newsReaderFactory);
 
 
+
+
+    }
 }
